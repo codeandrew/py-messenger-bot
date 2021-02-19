46fa3ff8d719d0dba8c3c6d74c54d161f508ed7a
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2021-02-19 21:10:27 +0800

    Initial app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Flask Setup
+import os
+import requests
+from datetime import datetime
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'application/json'
+
+verify_token = "hello"
+@app.route('/', methods=["GET"])
+@cross_origin()
+def verify():
+    # Webhook configuration
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == verify_token:
+            return "Verification token mismatch", 403
+    return "Hello World", 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 8080))
