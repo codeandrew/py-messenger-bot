71e9de841dfe05d9923d4468bcf4df64c4e5805a
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2021-02-20 12:57:14 +0800

    Working Echo Bot

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from datetime import datetime
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS, cross_origin
 from pprint import pprint
+from pymessenger import Bot
+
+PAGE_ACCESS_TOKEN = "ACCESSTOKEN"
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 app = Flask(__name__)
 CORS(app)
@@ -27,7 +31,23 @@ def webhook():
     data = request.get_json()
     log(data)
 
-    return "ok", 200
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                sender_id = messaging_event['sender']['id']
+                receipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text'] + " #echo"
+                    else:
+                        messaging_text = 'no text'
+
+                    # Echo 
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+    return "hello", 200
 
 def log(message):
     print(message)
